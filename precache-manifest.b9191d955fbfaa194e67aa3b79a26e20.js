75d23b9a3140b63754b2f3059bb6cce7ace66bb3
Debra Cupitt <debracupitt@gmail.com>
2019-02-12 10:08:49 +1100

    Updates

---
+++ precache-manifest.b9191d955fbfaa194e67aa3b79a26e20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b84e01100339f1d2626b",
+    "url": "/complete-guide-react/static/js/runtime~main.b84e0110.js"
+  },
+  {
+    "revision": "6b553b3fd4c5db52395e",
+    "url": "/complete-guide-react/static/js/main.6b553b3f.chunk.js"
+  },
+  {
+    "revision": "943237b24919d125cc12",
+    "url": "/complete-guide-react/static/js/1.943237b2.chunk.js"
+  },
+  {
+    "revision": "6b553b3fd4c5db52395e",
+    "url": "/complete-guide-react/static/css/main.41f54ea3.chunk.css"
+  },
+  {
+    "revision": "cd21dfb677e5ec86e5ce72b26aafb4e3",
+    "url": "/complete-guide-react/index.html"
+  }
+];
