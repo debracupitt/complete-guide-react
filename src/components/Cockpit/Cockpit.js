b4ebd0c28ddd5d40c6b70adf3b21137e6e016622
Debra Cupitt <debracupitt@gmail.com>
2019-02-12 16:06:15 +1100

    Add life-cycle hooks - rendering or creation hooks

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,39 @@
+import React from "react";
+import classes from "./Cockpit.css";
+
+const cockpit = props => {
+  const assignedClasses = [];
+  let btnClass = "";
+  let description = "There's lots of people here.";
+
+  if (props.showPersons) {
+    btnClass = classes.pink;
+  }
+
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red);
+    description = "There's not many people left...";
+  }
+
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold);
+    description = "There's only one person left!";
+  }
+
+  if (props.persons.length <= 0) {
+    assignedClasses.push(classes.darkred);
+    description = "THERE'S NO ONE LEFT!";
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(" ")}>{description}</p>
+      <button className={btnClass} onClick={props.clicked}>
+        Show/Hide Names
+      </button>
+    </div>
+  );
+};
+
+export default cockpit;
---
+++ src/containers/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import classes from "./App.css";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] construtor");
+  }
+  state = {
+    persons: [
+      { id: "kljsdf", name: "Deb", age: 27 },
+      { id: "yernfj", name: "Tim", age: 28 },
+      { id: "mvkshd", name: "Claire", age: 61 }
+    ],
+    showPersons: false
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps", props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log("[App.js] componentDidMount");
+  }
+
+  deletePersonHandler = personIndex => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    console.log("[App.js] render");
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
+      );
+    }
+
+    return (
+      <div className={classes.App}>
+        <Cockpit
+          title={this.props.appTitle}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonHandler}
+        />
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
