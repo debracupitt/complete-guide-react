1983c1902b5ab7ce03953901ebeca1e8b2e8457a
Debra Cupitt <debracupitt@gmail.com>
2019-02-13 17:45:25 +1100

    add refs to person component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,9 +1,19 @@
 import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+
 import Aux from "../../../hoc/Aux";
-import classes from "./Person.css";
 import withClass from "../../../hoc/withClass";
+import classes from "./Person.css";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log("[Person.js] rendering...");
     return (
@@ -14,6 +24,10 @@ class Person extends Component {
         <p key="i2">{this.props.children}</p>
         <input
           key="i3"
+          // ref={inputEl => {
+          //   this.inputElement = inputEl;
+          // }}
+          ref={this.inputElementRef}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
@@ -22,5 +36,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
