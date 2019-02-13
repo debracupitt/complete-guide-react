15b6e7d60870934619ac67a1666dfb816e4e9834
Debra Cupitt <debracupitt@gmail.com>
2019-02-13 17:22:20 +1100

    update the state correctly by using a function and referring to the previous state

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,8 @@ class App extends Component {
       { id: "yernfj", name: "Tim", age: 28 },
       { id: "mvkshd", name: "Claire", age: 61 }
     ],
-    showPersons: false
+    showPersons: false,
+    changeCounter: 0
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -57,7 +58,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      };
+    });
   };
 
   togglePersonHandler = () => {
